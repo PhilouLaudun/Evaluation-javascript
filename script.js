6b79cc9f9a8090bffa1e86b657d203a269202ddb
PhilouLaudun <77538675+PhilouLaudun@users.noreply.github.com>
2021-03-18 18:44:46 +0100

    Debut programmation js : fonction nombre alétoire

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const Scoreglob1 = $('#Global1') /* score global du joueur 1 */
 const Scorecoub1 = $('#Scourant1') /* score courant du joueur 1 */
 const Scoreglob2 = $('#Global2') /* score global du joueur 2 */
 const Scorecoub2 = $('#Scourant2') /* score courant du joueur 2 */
-const Imagedes = $('imagedes')
+const Imagedes = $('#imagedes') /* image du dés */
 
-
+function generernombre (bornemax) {    
+    return Math.floor(Math.random() * (bornemax - .5) + 1);
+}
 function remiseazero() {
-    Scoreglob1.text('0')
-    Scorecoub1.text('0')
-    Scoreglob2.text('0')
-    Scorecoub2.text('0')
-    
+    Scoreglob1.text('0') /* met le score global du joueur 1 à zéro */
+    Scorecoub1.text('0') /* met le score courant du joueur 1 à zéro */
+    Scoreglob2.text('0') /* met le score global du joueur 2 à zéro */
+    Scorecoub2.text('0') /* met le score courant du joueur 1 à zéro */
+    Imagedes.attr('src','/image/Image des 3.png'); /* ! pour les essais on laisse à 3 mais pour le jeu on remettra à 1 */
+
+    /* pour l'heure on fait comme cela mais par la suite on utilisera les propriétés des joueurs qu'on affichera */
 } 
 
-remiseazero()
-$('imagedes').attr('src','image/Image des 3.png');
+/* remiseazero() */
+$("#btnRollDice").on("click", function() {
+    /* alert("The paragraph was clicked.");*/
+    const nombreAleatoire = generernombre(6);
+    alert(nombreAleatoire);
+})
